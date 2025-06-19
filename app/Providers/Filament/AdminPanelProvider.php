e71f0fcf2431caa5a6a9f6fc4a29239a0bef8e5c
Yogi <yogiwijaya117@gmail.com>
2025-06-19 14:30:21 +0700

    Perubahan pada tampilan dashboard admin dan dokter

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -1,4 +1,7 @@
 <?php
+// File: app/Providers/Filament/AdminPanelProvider.php
+// Update untuk include custom dashboard
+
 namespace App\Providers\Filament;
 
 use Filament\Http\Middleware\Authenticate;
@@ -33,12 +36,14 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Pages\Dashboard::class,
+                // Gunakan custom dashboard atau default
+                \App\Filament\Pages\AdminDashboard::class,
+                // Pages\Dashboard::class, // comment ini jika pakai custom
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
